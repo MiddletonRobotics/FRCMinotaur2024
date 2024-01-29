0607d2265edc291190b58e5d282e8a81a284fa29
ultimate_hecker <86735991+ultimatehecker@users.noreply.github.com>
2024-01-28 22:22:17 -0500

    changed constants location update

--- src/main/java/frc/robot/utilities/XboxController.java
+++ src/main/java/frc/robot/utilities/XboxController.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.utilities.constants.Constants;
 
 public class XboxController {
-    private static final CommandXboxController DriverController = new CommandXboxController(Constants.driverControllerPort);
-    private static final CommandXboxController OperatorController = new CommandXboxController(Constants.operatorControllerPort);
+    private static final CommandXboxController DriverController = new CommandXboxController(Constants.DriverConstants.driverControllerPort);
+    private static final CommandXboxController OperatorController = new CommandXboxController(Constants.DriverConstants.operatorControllerPort);
 
     public static CommandXboxController getDriverController() {
         return DriverController;        
