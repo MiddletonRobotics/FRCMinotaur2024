c10d7528c6613234c65aa4461504ec7393c75f30
ultimate_hecker <86735991+ultimatehecker@users.noreply.github.com>
2024-01-28 22:22:07 -0500

    swerve subsystem with navX

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,141 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CANcoderConfigurator;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import frc.robot.utilities.constants.Constants;
+import frc.robot.utilities.constants.Constants.ModuleConstants;
+
+public class SwerveModule {
+    private final CANSparkMax forwardMotor;
+    private final CANSparkMax thetaMotor;
+
+    private final RelativeEncoder forwardEncoder;
+    private final RelativeEncoder thetaEncoder;
+
+    private final PIDController SwervePIDController;
+
+    private final CANcoder swerveEncoder;
+
+    private final boolean swerveEncoderReversed;
+    private final double swerveEncoderOffset;
+
+    public SwerveModule(int forwardMotorID, int thetaMotorID, boolean forwardMotorReversed, boolean thetaMotorReversed, int absoluteEncoderID, double encoderOffset, boolean encoderReversed) {
+        this.swerveEncoderOffset = encoderOffset;
+        this.swerveEncoderReversed = encoderReversed;
+        swerveEncoder = new CANcoder(absoluteEncoderID);
+
+        CANcoderConfigurator configurator = swerveEncoder.getConfigurator();
+
+        MagnetSensorConfigs magnetSensorConfiguration = new MagnetSensorConfigs();
+        magnetSensorConfiguration.AbsoluteSensorRange = AbsoluteSensorRangeValue.Signed_PlusMinusHalf;
+
+        configurator.apply(new CANcoderConfiguration().withMagnetSensor(magnetSensorConfiguration));
+
+        forwardMotor = new CANSparkMax(forwardMotorID, MotorType.kBrushless);
+        thetaMotor = new CANSparkMax(thetaMotorID, MotorType.kBrushless);
+
+        forwardMotor.setInverted(forwardMotorReversed);
+        thetaMotor.setInverted(thetaMotorReversed);
+
+        /*
+
+        if(forwardMotorReversed = thetaMotorReversed) {
+            throw new Exception("The forward motors and theta motors can't be both reversed, please check Constants.java");
+        }
+
+        */
+
+        forwardEncoder = forwardMotor.getEncoder();
+        thetaEncoder = thetaMotor.getEncoder();
+
+        forwardEncoder.setPositionConversionFactor(Constants.ModuleConstants.DriveEncoderRot2Meter);
+        forwardEncoder.setVelocityConversionFactor(Constants.ModuleConstants.DriveEncoderRPM2MeterPerSec);
+        thetaEncoder.setPositionConversionFactor(Constants.ModuleConstants.TurningEncoderRot2Rad);
+        thetaEncoder.setVelocityConversionFactor(Constants.ModuleConstants.TurningEncoderRPM2RadPerSec);
+
+        SwervePIDController = new PIDController(ModuleConstants.kP, 0, 0);
+        SwervePIDController.enableContinuousInput(-Math.PI, Math.PI); // EXPERIMENTAL
+
+        resetMotorEncoders();
+    }
+
+    public double getForwardPosition() {
+        return forwardEncoder.getPosition();
+    }
+
+    public double getThetaPosition() {
+        return thetaEncoder.getPosition();
+    }
+
+    public double getForwardVelocity() {
+        return forwardEncoder.getVelocity();
+    }
+
+    public double getThetaVelocity() {
+        return thetaEncoder.getVelocity();
+    }
+
+    public double getCANCoderPosition() {
+        double angle = swerveEncoder.getSupplyVoltage().getValueAsDouble() / RobotController.getVoltage5V();
+
+        angle *= 2 * Math.PI;
+        angle -= swerveEncoderOffset;
+        return angle * (swerveEncoderReversed ? -1.0 : 1.0);
+    }
+
+    public void resetMotorEncoders() {
+        forwardEncoder.setPosition(0);
+        thetaEncoder.setPosition(getCANCoderPosition());
+    }
+
+    public SwerveModuleState getSwerveModuleState() {
+        return new SwerveModuleState(getForwardVelocity(), new Rotation2d(getThetaPosition()));
+    }
+
+    /* This could therodically work, although the swerve module state will reset the modules to 0 when an input is not given as per here: 
+
+    public void setDesiredState(SwerveModuleState desiredState) {
+        desiredState  = SwerveModuleState.optimize(desiredState, new Rotation2d(getCANCoderPosition()));
+        forwardMotor.set(desiredState.speedMetersPerSecond);
+        thetaMotor.set(SwervePIDController.calculate(getThetaPosition(), desiredState.angle.getRadians()));
+
+        SmartDashboard.putString("Swerve [" + swerveEncoder.getChannel() + "]", desiredState.toString());
+    }
+
+    */
+
+    public void setDesiredState(SwerveModuleState desiredState) {
+
+        if(Math.abs(desiredState.speedMetersPerSecond) < 0.001) {
+            stop();
+            return;
+        }
+
+        desiredState  = SwerveModuleState.optimize(desiredState, new Rotation2d(getCANCoderPosition()));
+        forwardMotor.set(desiredState.speedMetersPerSecond);
+        thetaMotor.set(SwervePIDController.calculate(getThetaPosition(), desiredState.angle.getRadians()));
+
+        SmartDashboard.putString("Swerve [" + swerveEncoder.getDeviceID() + "]", desiredState.toString());
+
+    }
+
+    public void stop() {
+        forwardMotor.set(0);
+        thetaMotor.set(0);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,98 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.SPI;
+
+import frc.robot.utilities.constants.Constants;
+
+public class SwerveSubsystem extends SubsystemBase {
+    private final SwerveModule FrontLeftSwerveModule = new SwerveModule(
+        Constants.SwerveConstants.FrontLeftForwardMotorID, 
+        Constants.SwerveConstants.FrontLeftThetaMotorID, 
+        Constants.SwerveConstants.FrontLeftForwardEncoderReversed, 
+        Constants.SwerveConstants.FrontLeftThetaEncoderReversed, 
+        Constants.SwerveConstants.FrontLeftCANcoderID, 
+        Constants.SwerveConstants.FrontLeftCANcoderOffsetRadians, 
+        Constants.SwerveConstants.FrontLeftCANcoderReversed
+    );
+
+    private final SwerveModule FrontRightSwerveModule = new SwerveModule(
+        Constants.SwerveConstants.FrontRightForwardMotorID, 
+        Constants.SwerveConstants.FrontRightThetaMotorID, 
+        Constants.SwerveConstants.FrontRightForwardEncoderReversed, 
+        Constants.SwerveConstants.FrontRightThetaEncoderReversed, 
+        Constants.SwerveConstants.FrontRightCANcoderID, 
+        Constants.SwerveConstants.FrontRightCANcoderOffsetRadians, 
+        Constants.SwerveConstants.FrontRightCANcoderReversed
+    );
+
+    private final SwerveModule BackLeftSwerveModule = new SwerveModule(
+        Constants.SwerveConstants.BackLeftForwardMotorID, 
+        Constants.SwerveConstants.BackLeftThetaMotorID, 
+        Constants.SwerveConstants.BackLeftForwardEncoderReversed, 
+        Constants.SwerveConstants.BackLeftThetaEncoderReversed, 
+        Constants.SwerveConstants.BackLeftCANcoderID, 
+        Constants.SwerveConstants.BackLeftCANcoderOffsetRadians, 
+        Constants.SwerveConstants.BackLeftCANcoderReversed
+    );
+
+    private final SwerveModule BackRightSwerveModule = new SwerveModule(
+        Constants.SwerveConstants.BackRightForwardMotorID, 
+        Constants.SwerveConstants.BackRightThetaMotorID, 
+        Constants.SwerveConstants.BackRightForwardEncoderReversed, 
+        Constants.SwerveConstants.BackRightThetaEncoderReversed, 
+        Constants.SwerveConstants.BackRightCANcoderID, 
+        Constants.SwerveConstants.BackRightCANcoderOffsetRadians, 
+        Constants.SwerveConstants.BackRightCANcoderReversed
+    );
+
+    private AHRS gyro = new AHRS(SPI.Port.kMXP);
+
+    public SwerveSubsystem() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+                zeroHeading();
+            } catch (Exception e) {}
+        }).start();
+    }
+
+    public void zeroHeading() {
+        gyro.reset();
+    }
+
+    public double getHeading() {
+        return Math.IEEEremainder(gyro.getAngle(), 360);
+    }
+
+    public Rotation2d geRotation2d() {
+        return Rotation2d.fromDegrees(getHeading());
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Robot Heading", getHeading());
+    }
+
+    public void stopSwerveModules() {
+        FrontLeftSwerveModule.stop();
+        FrontRightSwerveModule.stop();
+        BackLeftSwerveModule.stop();
+        BackRightSwerveModule.stop();
+    }
+
+    public void setSwerveModuleStates(SwerveModuleState[] desiredStates) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.SwerveConstants.PhysicalMaxSpeedMetersPerSecond);
+
+        FrontLeftSwerveModule.setDesiredState(desiredStates[0]);
+        FrontRightSwerveModule.setDesiredState(desiredStates[1]);
+        BackLeftSwerveModule.setDesiredState(desiredStates[2]);
+        BackRightSwerveModule.setDesiredState(desiredStates[3]);
+    }
+}
