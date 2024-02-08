7a1a4a273458ad2a3554a6dd4024330a4e2803bf
Sabrina S <55033754+Robokid12@users.noreply.github.com>
2024-02-08 16:13:11 -0500

    Update Controller.java with comments
    Record information on functionality for future generations (may be edited if details are needed).

--- src/main/java/frc/robot/utilities/Controller.java
+++ src/main/java/frc/robot/utilities/Controller.java
@@ -1,8 +1,11 @@
+// Import information on controls (joystick, buttons) to set up each operation.
+
 package frc.robot.utilities;
 
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.utilities.constants.Constants;
 
+//Setup class that connects buttons/joysticks on controller to code. RobotContainer needs this.
 public class Controller {
     private static final Joystick DriverController = new Joystick(Constants.DriverConstants.driverControllerPort);
     private static final Joystick OperatorController = new Joystick(Constants.DriverConstants.operatorControllerPort);
