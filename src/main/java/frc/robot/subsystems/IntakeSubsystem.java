4c82993c54af129805de21f6fa63db1421fad6bd
ArinMotte <160786106+ArinMotte@users.noreply.github.com>
2024-02-28 16:41:44 -0500

    Basic stuff done for IntakeSubsystem.java
    Added rotation motor, put placeholder values down, fixed up the code generally (removed unnecessary code) and commented it.

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -9,74 +9,19 @@ import frc.robot.Utilities.Section;
 import com.revrobotics.CANSparkMax;
 
 
-public class Intake extends Subsystem implements Section, Constants {
+public class Intake extends Subsystem {
 
     private CANSparkMax topIntake;
     private CANSparkMax bottomIntake;
+    private CANSparkMax rotateIntake;
 
 
     private static Intake instance = null;
 
     private Intake() {
-        topIntake = new CANSparkMax​(intakeTopID, intakeTopMotorType);
-        bottomIntake = new CANSparkMax(intakeBottomID, intakeBottomMotorType);
-
-        configTalons();
-
-        closeIntake();
-    }
-
-    public static Intake getInstance() {
-        if(instance == null) {
-            instance = new Intake();
-        }
-
-        return instance;
-    }
-
-    private void configTalons() {
-        topIntake.set(0.0);
-        bottomIntake.set(0.0);
-        topIntake.configOpenloopRamp(0, 0);
-        bottomIntake.configOpenloopRamp(0, 0);
-        bottomIntake.setNeutralMode(NeutralMode.Brake);
-        topIntake.setNeutralMode(NeutralMode.Brake);
-
-    }
-
-    @Override
-    protected void initDefaultCommand() {
-        System.out.println("yeah this thing sucks");
-    }
-
-    private boolean intaking = false;
-    @Override
-    public void teleop(MinoGamepad gamepad) {
-
-/*
-        if(gamepad.bottomTriggerPressed()) {
-            intaking = false;
-            topIntake.configOpenloopRamp(0, kTimeoutMs);
-            bottomIntake.configOpenloopRamp(0, kTimeoutMs);
-        } else if (gamepad.topTriggerPressed()){
-            intaking = true;
-            topIntake.configOpenloopRamp(0.25, kTimeoutMs);
-            bottomIntake.configOpenloopRamp(0.25, kTimeoutMs);
-            setPercentSpeed(intakeSolenoid.getValue() == DoubleSolenoid.Value.kForward ? 1 : -1); //IN
-        } else {
-            if (intaking) {
-                setPercentSpeed(intakeSolenoid.getValue() == DoubleSolenoid.Value.kForward ? 0.05 : -0.05); //IN
-            } else {
-                topIntake.set(0);
-                bottomIntake.set(0);
-            }
-
-        }
-*/
-        if (gamepad.getRawButton(BTN_X)) {
-            toggleIntake();
-        }
-
+        topIntake = new CANSparkMax​(62 intakeTopID, CANSparkLowLevel.MotorType.kBrushless);
+        bottomIntake = new CANSparkMax(7 intakeBottomID, CANSparkLowLevel.MotorType.kBrushless);
+        rotateIntake = new CANSparkMax(48 intakeRotateID, CANSparkLowLevel.MotorType.kBrushless); //ints are just there as placeholders till we get actual ones, same with ids
     }
 
 //we felt a little silly with the names
@@ -90,19 +35,18 @@ public class Intake extends Subsystem implements Section, Constants {
         bottomIntake.set(-0.5); //make it match bottom
     }
 
-    public void toggleIntake() {
-        
+    public void intakeForward() {
+        rotateIntake.set(0.5); //placeholder values for rn
+    }
+
+    public void intakeBackward() {
+        rotateIntake.set(-0.5); //same comment as intakeForward
     }
 
     @Override
     public void reset() {
         topIntake.set(0);
         bottomIntake.set(0);
-        closeIntake();
     }
 
-    public void setPercentSpeed(double speed) {
-        topIntake.set(-speed);
-        bottomIntake.set(speed);
-    }
 }
