966ca7e813aa164f43d05d4fbec0bb8471b807d6
ultimate_hecker <86735991+ultimatehecker@users.noreply.github.com>
2024-03-04 19:33:29 -0500

    base code for intake

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utilities.constants.Constants;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+    public CANSparkMax rollerMotor;
+    public CANSparkMax pivotMotor;
+
+    public static IntakeSubsystem instance = null;
+
+    public IntakeSubsystem() {
+        rollerMotor = new CANSparkMax(Constants.IntakeConstants.rollerMotorID, MotorType.kBrushless);
+        pivotMotor = new CANSparkMax(Constants.IntakeConstants.pivotMotorID, MotorType.kBrushless);
+    }
+
+
+    public void configureRollerMotor() {
+
+    }
+
+    public void configurePivotMotor() {
+
+    }
+
+    @Override
+    public void periodic() {
+        
+    }
+}
