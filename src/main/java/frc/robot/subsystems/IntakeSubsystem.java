b6413235b5be3910f877877d94b78bfc6798dfa3
ultimate_hecker <86735991+ultimatehecker@users.noreply.github.com>
2024-03-06 09:58:30 -0500

    fixed intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,21 +2,19 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Utilities.Constants.Constants;
-import frc.robot.Utilities.Drivers.MinoGamepad;
-import frc.robot.Utilities.Section;
+import frc.robot.utilities.constants.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 
-public class Intake extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase {
 
     public CANSparkMax rollerMotor;
     public CANSparkMax pivotMotor;
 
-    public Intake() {
-        rollerMotor = new CANSparkMax(Constants.IntakeConstants.rollerMotorID, CANSparkLowLevel.MotorType.kBrushless);
-        pivotMotor = new CANSparkMax(Constants.IntakeConstants.pivotMotorID, CANSparkLowLevel.MotorType.kBrushless); //ints are just there as placeholders till we get actual ones, same with ids
+    public IntakeSubsystem() {
+        rollerMotor = new CANSparkMax(Constants.IntakeConstants.rollerMotorID, MotorType.kBrushless);
+        pivotMotor = new CANSparkMax(Constants.IntakeConstants.pivotMotorID, MotorType.kBrushless); //ints are just there as placeholders till we get actual ones, same with ids
     }
 
 //we felt a little silly with the names
@@ -35,11 +33,6 @@ public class Intake extends SubsystemBase {
     public void intakeBackward() {
         pivotMotor.set(-0.5); //same comment as intakeForward
     }
-
-    @Override
-    public void reset() {
-        rollerMotor.set(0);
-    }
   
     public void configureRollerMotor() {
 
@@ -47,6 +40,10 @@ public class Intake extends SubsystemBase {
 
     public void configurePivotMotor() {
 
+    }
+
+    public void reset() {
+        rollerMotor.set(0);
     }
 
     @Override
