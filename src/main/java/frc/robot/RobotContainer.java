5ef7c5aa0bc3f7853198487782c61c1f9ab1c0da
Sabrina S <55033754+Robokid12@users.noreply.github.com>
2024-03-18 18:59:03 -0400

    fixed controls momentarily

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -101,8 +101,8 @@ public class RobotContainer {
     deployIntake = new JoystickButton(OperatorController, Constants.ControllerRawButtons.Button.kA.value);
     storeIntake = new JoystickButton(OperatorController, Constants.ControllerRawButtons.Button.kB.value);
     intakeGamePiece = new JoystickButton(OperatorController, Constants.ControllerRawButtons.Button.kX.value);
-    robotClimbUp = new JoystickButton(OperatorController, Constants.ControllerRawButtons.Button.kLeftTrigger.value);
-    robotClimbDown = new JoystickButton(OperatorController, Constants.ControllerRawButtons.Button.kRightTrigger.value);
+    robotClimbUp = new JoystickButton(OperatorController, Constants.ControllerRawButtons.Button.kStart.value);
+    robotClimbDown = new JoystickButton(OperatorController, Constants.ControllerRawButtons.Button.kBack.value);
 
     translationAxis = Constants.ControllerRawButtons.Axis.kLeftY.value;
     strafeAxis = Constants.ControllerRawButtons.Axis.kLeftX.value;
